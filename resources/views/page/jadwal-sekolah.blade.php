0e44a6cf2824c099395f6d746152c38b2e9a154a
Zeus-s <kristabdixx@gmail.com>
2020-12-28 19:47:30 +0700

    roadshow content fix 3

--- resources/views/page/jadwal-sekolah.blade.php
+++ resources/views/page/jadwal-sekolah.blade.php
@@ -24,11 +24,11 @@
               <h1> {{ trans('menu.schedule_school') }} </h1>
               <h2> <strong> Roadshow KPGTS hadir kembali tahun ini! </strong> </h2>
               <p> <strong> Acara Roadshow ini apa sih? </strong> <br> 
-              Jadi, roadshow adalah salah satu rangkaian acara dari KPGTS untuk mengenalkan Institut Teknologi Bandung kepada para siswa  kelas 12 SMA sederajat. Diharapkan acara ini bisa membantu teman-teman untuk mendapat gambaran yang lebih jelas tentang fakultas dan semua hal yang berkaitan dengan ITB! <br> </p>
+              Jadi, roadshow adalah salah satu rangkaian acara dari KPGTS untuk mengenalkan Institut Teknologi Bandung kepada para siswa  kelas 12 SMA sederajat. Diharapkan acara ini bisa membantu teman-teman untuk mendapat gambaran yang lebih jelas tentang fakultas dan semua hal yang berkaitan dengan ITB! <br> &nbsp; </p>
               <p> <strong> Kalau tahun ini roadshownya gimana kak? </strong> <br>
-              Nah, tahun ini  Roadshow KPGTS bakal sedikit berbeda dengan tahun sebelumnya karena acaranya  akan dilakukan melalui platform zoom! Jadi, kita bisa ngikutin acaranya di rumah masing-masing!! <br> </p>
+              Nah, tahun ini  Roadshow KPGTS bakal sedikit berbeda dengan tahun sebelumnya karena acaranya  akan dilakukan melalui platform zoom! Jadi, kita bisa ngikutin acaranya di rumah masing-masing!! <br> &nbsp; </p>
               <p> <strong> Kapan sih kak Roadshownya? </strong> <br>
-              Roadshownya bakal dilaksanain tanggal 15 Januari 2021. Buat waktu dan linknya bakal menyusul. Jadi stay tune ya buat kabar berikutnya!! <br> </p>
+              Roadshownya bakal dilaksanain tanggal 15 Januari 2021. Buat waktu dan linknya bakal menyusul. Jadi stay tune ya buat kabar berikutnya!! <br> &nbsp; &nbsp; </p>
               <strong> Yakali kalian gak ikut? </strong> <br>
               <strong> Yuk langsung daftar aja!! </strong> 
 
